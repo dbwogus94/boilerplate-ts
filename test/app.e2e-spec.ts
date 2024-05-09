38ef8261d356bd5bc9209616044222d13f266bf3
jae hyun <1994dbwogus@gmail.com>
2024-05-09 22:51:52 +0900

    feat: 초기 셋팅

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,12 @@
+// import * as request from 'supertest';
+
+// describe('AppController (e2e)', () => {
+//   beforeEach(async () => {});
+
+//   it('/ (GET)', () => {
+//     return request(app.getHttpServer())
+//       .get('/')
+//       .expect(200)
+//       .expect('Hello World!');
+//   });
+// });
